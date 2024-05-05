da1da3f075d329d333354b96bd7a0a4ac0613b75
Harshit <pharshit2003@gmail.com>
2024-05-05 22:54:08 +0530

    map added

--- seeds/index.js
+++ seeds/index.js
@@ -21,11 +21,16 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            author: '661ab18ec6c1eb520ab9e4ba',
+            //YOUR USER ID
+            author:'661ab18ec6c1eb520ab9e4ba',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price,
+            geometry: {
+                type: "Point",
+                coordinates: [-113.1331, 47.0202]
+            },
             images: [
                 {
                     url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
@@ -35,12 +40,11 @@ const seedDB = async () => {
                     url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ruyoaxgf72nzpi4y6cdi.png',
                     filename: 'YelpCamp/ruyoaxgf72nzpi4y6cdi'
                 }
-            ]  
+            ]
         })
         await camp.save();
     }
 }
-https://res.cloudinary.com/dew3bk0yv/image/upload/v1714184085/YelpCamp/eetneopykuzfbevrkwql.jpg
 
 seedDB().then(() => {
     mongoose.connection.close();
